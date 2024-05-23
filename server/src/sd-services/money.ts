6ef7ae955d433a241bdf7e305e0eac5470e5d69f
Nicholas <lesetjamaluleka@gmail.com>
2024-05-23 18:20:40 +0200

    fixed send money api

--- server/src/sd-services/money.ts
+++ server/src/sd-services/money.ts
@@ -646,8 +646,8 @@ export class money {
       };
 
       bh.input.body['_id'] = new Date().getTime();
-      bh.input.body['money-in'] = 'payment received';
-      bh.input.body['money-out'] = 'fees';
+      bh.input.body['moneyIn'] = 'payment received';
+      bh.input.body['moneyOut'] = 'fees';
 
       this.tracerService.sendData(spanInst, bh);
       bh = await this.sd_Pw4KZCHOFtLfdzC8(bh, parentSpanInst);
